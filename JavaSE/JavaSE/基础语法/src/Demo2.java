f7442b67bdca3c8074b0a22da74c74f0ba3396ab
monkey0901 <1284757581@qq.com>
2022-04-08 21:54:01 +0800

    数据类型学习

---
+++ JavaSE/JavaSE/基础语法/src/Demo2.java
@@ -0,0 +1,27 @@
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+public class Demo2 {
+
+    public static void main(String[] args) {
+        // 数字类型练习
+        // 八大数字类型
+
+        // 整数
+        int num1 = 1;
+        short num2 = 20;
+        byte num3 = 30;
+        long num4 = 30L;
+
+        // 浮点数
+        float num5 = 40F;
+        double num6 = 50;
+
+        // 字符型
+        char a = 'u';
+//        字符串， string不是关键字是类
+//        String str= "test";
+
+        // 布尔型
+        boolean flag = true;
+    }
+}
---
+++ JavaSE/JavaSE/基础语法/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // 单行注释
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        /*
+        多行注释
+         */
+
+    }
+}
